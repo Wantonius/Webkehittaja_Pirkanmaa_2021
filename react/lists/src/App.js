43c940fc37d0db01bc7d59981dca4f208457581c
Erno Hentonen <erno.hentonen@opiframe.com>
2021-01-26 11:03:35 +0200

    lists

--- react/lists/src/App.js
+++ react/lists/src/App.js
@@ -1,20 +1,42 @@
 import logo from './logo.svg';
 import './App.css';
 import React from 'react';
+import ContactForm from './ContactForm';
+import ContactList from './ContactList';
 
 class App extends React.Component {
 
 	constructor(props) {
 		super(props);
 		this.state = {
-			list:[]
+			list:[],
+			id:100
 		}
+	}
+	
+	addToList = (contact) => {
+		contact.id = this.state.id;
+		this.setState((state) => ({
+				list:state.list.concat(contact),
+				id:state.id+1
+		}));		
+	}
+	
+	removeFromList = (id) => {
+		this.setState((state) => {
+			let tempList = state.list.filter(contact => contact.id !== id)
+			return {
+				list:tempList
+			}
+		})
 	}
 
 	render() {
 		return (
 			<div className="App">
-
+				<ContactForm addToList={this.addToList}/>
+				<hr/>
+				<ContactList list={this.state.list} removeFromList={this.removeFromList}/>
 			</div>
 		);
 	}
