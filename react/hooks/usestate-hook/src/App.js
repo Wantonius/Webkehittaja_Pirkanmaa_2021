8225af342ce29171320a69b780c78be1aefc6ed1
Erno Hentonen <erno.hentonen@opiframe.com>
2021-03-10 12:33:04 +0200

    usestate-hook

---
+++ react/hooks/usestate-hook/src/App.js
@@ -0,0 +1,43 @@
+import logo from './logo.svg';
+import './App.css';
+import {useState} from 'react';
+import ShoppingForm from './ShoppingForm';
+import ShoppingList from './ShoppingList';
+
+function App() {
+	
+	const [state,setState] = useState({
+		list:[],
+		id:100
+	});
+ 	
+	const addToList = (item) => {
+		item.id = state.id;
+		setState(state => {
+			return {
+				list:state.list.concat(item),
+				id:state.id+1
+			}
+		})
+	}
+	
+	const removeFromList = (id) => {
+		setState(state => {
+			let tempList = state.list.filter(item => item.id !== id)
+			return {
+				...state,
+				list:tempList
+			}
+		})
+	}
+	
+	return (
+		<div className="App">
+			<ShoppingForm addToList={addToList}/>
+			<hr/>
+			<ShoppingList list={state.list} removeFromList={removeFromList}/>
+		</div>
+	);
+}
+
+export default App;
